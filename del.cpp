63b4c9d7f4dd44142419d5c9805cf865355842fe
epinefri <danielasorinap@gmail.com>
2022-12-04 21:27:56 +0200

    new file:   del.cpp

---
+++ del.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v[] = {1, 2, 3, 4, 5};
+    int input;
+
+    cout << "delete: ";
+    cin >> input;
+
+    for (int i = input - 1; i < 5; i++)
+        v[i] = v[i + 1];
+
+    cout << "new: " << endl;
+    for (int i = 0; i < 4; i++)
+        cout << v[i];
+
+    cout << endl;
+}
