4471a5ac61a341e3a6f374dc976b91df22614a35
epinefri <danielasorinap@gmail.com>
2022-12-11 13:19:26 +0200

    modified:   matrix 	modified:   matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -5,16 +5,23 @@ int main()
 {
     int a[4][5], i, j;
 
-    int d = 20;
+    a[0][0] = 20;
     
     for(j=0; j<5; j++)
     {
         for(i=0; i<4; i++)
         {
-            a[i][j] = d;
-            d--;
-            cout << a[i][j] << " ";
+            a[i][j] = a[0][0];
+            a[0][0]--;
         }
-    cout << endl;
     }
+    a[0][0] = 20;
+
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<5; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+
 }
